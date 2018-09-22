060c02689aca1cc6c31330a7dbeddae894550307
seaflying88 <seaflying@live.cn>
2018-09-22 13:02:34 +0800

    增加网络模块Opcode

---
+++ IdleLikeAdventure/Server/NetData/NetData/OpCode/OpCodeNetOperation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetData.OpCode
+{
+    /// <summary>
+    /// 二级协议：角色模块
+    /// </summary>
+    public enum OpCodeNetOperation
+    {
+    }
+}
