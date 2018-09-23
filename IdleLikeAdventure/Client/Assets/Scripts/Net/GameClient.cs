9dd5bed38a20f2643fb3f1240db3fe9cf10a0353
seaflying <seaflying@live.cn>
2018-09-23 18:31:33 +0800

    网络客户端，调用Log接口

--- IdleLikeAdventure/Client/Assets/Scripts/Net/GameClient.cs
+++ IdleLikeAdventure/Client/Assets/Scripts/Net/GameClient.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 using ExitGames.Client.Photon;
 using NetData.Message;
 using NetData.OpCode;
+using Log;
 
 namespace Net
 {
@@ -32,7 +33,7 @@ namespace Net
             this.m_ClientState = ClientState.DisConnect;
             m_PhotonPeer = new PhotonPeer(this, ConnectionProtocol.Tcp);
             m_PhotonPeer.Connect(m_Serverhost, m_ServerName);     //链接服务器
-            Debug.Log("连接服务器中......");
+            TLog.LogInput("连接服务器中", Level.Low, false);
 
 
             //启动异步客户端服务
@@ -55,7 +56,7 @@ namespace Net
 
         public void OnOperationResponse(OperationResponse operationResponse)
         {
-            Debug.Log("++++++++++有消息");
+            TLog.LogInput("接收到服务器处理的返回消息", Level.Low, false);
 
             if (m_ResponseHandler != null)
             {
@@ -69,9 +70,10 @@ namespace Net
             {
                 case StatusCode.Connect:
                     m_ClientState = ClientState.Connect;
-                    Debug.Log("++++++++++连接上");
+                    TLog.LogInput("连接上服务器", Level.Low, false);
                     break;
                 case StatusCode.Disconnect:
+                    TLog.LogInput("与服务器断开连接", Level.High, false);
                     m_ClientState = ClientState.DisConnect;
                     break;
                 default:
