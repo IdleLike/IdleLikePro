635222491e3563e04f88029d8700810f8fadf9da
seaflying88 <seaflying@live.cn>
2018-09-24 19:49:22 +0800

    定义登陆注册消息消息

--- IdleLikeAdventure/Server/NetData/NetData/Message/BaseMsgData.cs
+++ IdleLikeAdventure/Server/NetData/NetData/Message/BaseMsgData.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NetData.OpCode;
+﻿using NetData.OpCode;
 
 namespace NetData.Message
 {
     public class BaseMsgData
     {
-        public ErrorCode Error { get; set; }
+        public bool IsError = false;
+        public ErrorCode Error = 0;
     }
 }
