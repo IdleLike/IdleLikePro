bd724ec0e83eb75066a7261f35a78ec3886d6bbd
seaflying88 <seaflying@live.cn>
2018-09-24 18:52:31 +0800

    增加登陆注册ErrorCode

--- IdleLikeAdventure/Server/NetData/NetData/OpCode/ErrorCode.cs
+++ IdleLikeAdventure/Server/NetData/NetData/OpCode/ErrorCode.cs
@@ -10,6 +10,12 @@ namespace NetData.OpCode
     /// </summary>
     public enum ErrorCode : uint
     {
+        //  登陆注册
+        LoginAccountError,              //登陆账号错误
+        LoginPasswordError,             //登陆密码错误
+        RegisterAccountError,           //注册账号错误
+        RegisterAccountExist,           //注册账号已存在
+        RegisterPasswordError,          //注册密码错误
 
     }
 }
