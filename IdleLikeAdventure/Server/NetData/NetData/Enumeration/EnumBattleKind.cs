b1593d76fa1cdd1679cdccb91a6d6c1c0270f362
seaflying88 <seaflying@live.cn>
2018-12-02 11:31:36 +0800

    战斗协议

---
+++ IdleLikeAdventure/Server/NetData/NetData/Enumeration/EnumBattleKind.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetData.Enumeration
+{
+    public enum EnumBattleKind
+    {
+        None,           //无
+        Normal,         //普通战斗
+        Hutting,        //狩猎
+        PvP,
+        Zoon            //副本
+    }
+}
