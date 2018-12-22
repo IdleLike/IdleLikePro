5a458e3fc21f363fbf9d0ba30686c1ea03e3a61f
seaflying88 <seaflying@live.cn>
2018-12-22 22:41:07 +0800

    修改网络消息类

--- IdleLikeAdventure/Server/IdleLikeAdventureServer/IdleLikeAdventureServer/Handler/BattleHandler.cs
+++ IdleLikeAdventure/Server/IdleLikeAdventureServer/IdleLikeAdventureServer/Handler/BattleHandler.cs
@@ -19,7 +19,42 @@ namespace IdleLikeAdventureServer.Handler
 
         public override void OnOperationRequest(BaseMsgData baseMsgData, SendParameters sendParameters, ClientPeer peer)
         {
-            
+            BattleRequestMsgData battleRequestMsgData = baseMsgData as BattleRequestMsgData;
+
+
+            BattleRespondeMsgData battleRespondeMsgData = new BattleRespondeMsgData();
+
+            battleRespondeMsgData.IsError = false;
+            battleRespondeMsgData.FindEnemyTime = 15;
+            BattleMsgData battleMsgData = new BattleMsgData();
+            battleMsgData.TeamID = battleRequestMsgData.TeamID;
+            battleMsgData.EnemyID = new List<uint>() {1001,1002,1003};
+            battleMsgData.Exps = new List<int>() { 100, 200, 500 };
+            battleMsgData.GoldRewards = 1000;
+            battleMsgData.IsWin = true;
+            battleMsgData.ItemRewards = new List<uint>() { 1001, 1002 };
+            battleMsgData.RestTime = 10;
+            battleMsgData.Rounds = new List<RoundInfoMsgData>();
+            for (int i = 0; i < 10; i++)
+            {
+                RoundInfoMsgData roundInfoMsgData = new RoundInfoMsgData();
+
+                roundInfoMsgData.AttackInfos = new List<AttackInfoMsgData>();
+
+                for (byte j = 0; j < 6; j++)
+                {
+                    AttackInfoMsgData attackInfoMsgData = new AttackInfoMsgData();
+
+                    attackInfoMsgData.AttakPos = j;
+                    attackInfoMsgData.DefendsPos = j % 2 == 0 ? (byte)(j + 1) : (byte)(j - 1);
+                    attackInfoMsgData.AbilityID = 0;
+                    attackInfoMsgData.AbiilityEffectValue = 10;
+                }
+
+                battleMsgData.Rounds.Add(roundInfoMsgData);
+            }
+
+            SendResponse(peer, sendParameters, battleRespondeMsgData);            
         }
     }
 }
--- IdleLikeAdventure/Server/NetData/NetData/Message/PlayerMsgData.cs
+++ IdleLikeAdventure/Server/NetData/NetData/Message/PlayerMsgData.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 namespace NetData.Message
 {
+    [Serializable]
     public class PlayerMsgData
     {
         public int DatabaseID;
