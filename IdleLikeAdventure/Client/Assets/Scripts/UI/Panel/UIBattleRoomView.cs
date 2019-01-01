37f249c9406f504e8d492899d35d85c92d45e1ca
FSH943503596 <38723993+FSH943503596@users.noreply.github.com>
2019-01-01 19:10:19 +0800

    1/3 功能界面 技能、BUff表

--- IdleLikeAdventure/Client/Assets/Scripts/UI/Panel/UIBattleRoomView.cs
+++ IdleLikeAdventure/Client/Assets/Scripts/UI/Panel/UIBattleRoomView.cs
@@ -106,8 +106,11 @@ public class UIBattleRoomView : BaseUIForm
     private string m_AbilityAttack = "{0}.{1}使用技能{2}，攻击了{3}";
     private string m_Dodge = "，但被对方闪避，{0}没有受到直接伤害";
     private string m_FailureResult = "{0}被击败，战斗结束...";
-    private string m_SuccessResult = "\n敌方被击败，战斗结束...\n";
-    private string m_Beat = "{0}被击败了";
+    private string m_SuccessResult = "\n敌方被击败，战斗结束...";
+    private string m_Beat = "\n{0}被击败了";
+    private string m_GetGold = "\n团队收获金币{0}g\n";
+    private string m_GetExp = "{0}获得了经验{1}Exp\n";
+    private string m_GetEquipment = "获得装备{0}\n";
 
     #endregion
 
@@ -135,6 +138,7 @@ public class UIBattleRoomView : BaseUIForm
         {
             InitCharcterInfo();
             int FindEnemyTime = m_ViewModel.BattleReportList.FindEnemyTime;
+            FindEnemyTime = 3;
             Debug.Log("FindEnemyTime : " + FindEnemyTime);
             if (FindEnemyTime > 0)
             {
@@ -169,7 +173,7 @@ public class UIBattleRoomView : BaseUIForm
     /// <param name="text"></param>
     /// <param name="slider"></param>
     /// <param name="type"></param>
-    void HPValueChanged(ref int hp,int maxhp,int effectValue,Text text,Slider slider, EnumAbilityEffect type)
+    void HPValueChanged(ref int hp,int maxhp,int effectValue,Text text,Slider slider, EnumAbilityEffect type,Text reportText,string name)
     {
         switch (type)
         {
@@ -186,6 +190,10 @@ public class UIBattleRoomView : BaseUIForm
         hp = hp > maxhp ? maxhp : hp; 
         text.text = hp.ToString();
         slider.value = Mathf.Clamp(hp, 0, maxhp) / (float)maxhp;
+        if (hp <= 0)
+        {
+            reportText.text += string.Format(m_Beat, name);
+        }
     }
     /// <summary>
     /// 获取战报信息
@@ -193,28 +201,50 @@ public class UIBattleRoomView : BaseUIForm
     /// <param name="info"></param>
     void GetAttackInfo(UIBattleRoomViewModel.AttackInfos info)
     {
+        AttackEnemy = null;
+        UIAttackEnemy = null;
+        AttackCharacter = null;
+        UIAttackCharacter = null;
+        DefendsEnemy = null;
+        UIDefendsEnemy = null;
+        DefendsCharacter = null;
+        UIDefendsCharacter = null;
+
+        int DefendsPos = 0;
+        int AttakPos = 0;
+
+        Log("AttakPos = " + AttakPos);
+        Log("DefendsPos = " + DefendsPos);
+
         if (info.AttakPos % 2 == 0)
         {
-            AttackEnemy = m_ViewModel.enemyList.Find(p => p.ID == info.AttakPos);
-            UIAttackEnemy = m_EnemyUIInfoList.Find(p => p.ID == info.AttakPos);
+            AttakPos = info.AttakPos / 2 - 1;
+            AttackEnemy = m_ViewModel.enemyList[AttakPos];
+            UIAttackEnemy = m_EnemyUIInfoList[AttakPos];
         }
         else
         {
-            AttackCharacter = m_ViewModel.characterList.Find(p => p.HeroID == info.AttakPos);
-            UIAttackCharacter = m_CharacterUIInfoList.Find(p => p.ID == info.AttakPos);
+            AttakPos = info.AttakPos / 2;
+            AttackCharacter = m_ViewModel.characterList[AttakPos];
+            UIAttackCharacter = m_CharacterUIInfoList[AttakPos];
         }
         if (info.DefendsPos % 2 == 0)
         {
-            DefendsEnemy = m_ViewModel.enemyList.Find(p => p.ID == info.DefendsPos);
-            UIDefendsEnemy = m_EnemyUIInfoList.Find(p => p.ID == info.DefendsPos);
+            DefendsPos = info.DefendsPos / 2 - 1;
+
+            DefendsEnemy = m_ViewModel.enemyList[DefendsPos];
+            UIDefendsEnemy = m_EnemyUIInfoList[DefendsPos];
         }
         else
         {
-            DefendsCharacter = m_ViewModel.characterList.Find(p => p.HeroID == info.DefendsPos);
-            UIDefendsCharacter = m_CharacterUIInfoList.Find(p => p.ID == info.DefendsPos);
+            DefendsPos = info.DefendsPos / 2;
+
+            DefendsCharacter = m_ViewModel.characterList[DefendsPos];
+            UIDefendsCharacter = m_CharacterUIInfoList[DefendsPos];
         }
         m_AttackName = AttackCharacter != null ? AttackCharacter.Name : AttackEnemy.Name;
         m_DefendsName = DefendsCharacter != null ? DefendsCharacter.Name : DefendsEnemy.Name;
+
         m_AbiilityEffectValue = info.AbiilityEffectValue;
         m_AbilityName = info.AbilityName;
     }
@@ -272,11 +302,11 @@ public class UIBattleRoomView : BaseUIForm
                     }
                     if (DefendsCharacter != null)
                     {
-                        HPValueChanged(ref DefendsCharacter.CurrentHP, DefendsCharacter.MaxHP, m_AbiilityEffectValue, UIDefendsCharacter.CurrentHP_Txt, UIDefendsCharacter.HP_Sli, AttackInfos[i].EffectType);
+                        HPValueChanged(ref DefendsCharacter.CurrentHP, DefendsCharacter.MaxHP, m_AbiilityEffectValue, UIDefendsCharacter.CurrentHP_Txt, UIDefendsCharacter.HP_Sli, AttackInfos[i].EffectType, m_ReportScript.m_Info_Txt, m_DefendsName);
                     }
                     else
                     {
-                        HPValueChanged(ref DefendsEnemy.CurrentHP, DefendsEnemy.MaxHP, m_AbiilityEffectValue, UIDefendsEnemy.CurrentHP_Txt, UIDefendsEnemy.HP_Sli, AttackInfos[i].EffectType);
+                        HPValueChanged(ref DefendsEnemy.CurrentHP, DefendsEnemy.MaxHP, m_AbiilityEffectValue, UIDefendsEnemy.CurrentHP_Txt, UIDefendsEnemy.HP_Sli, AttackInfos[i].EffectType, m_ReportScript.m_Info_Txt, m_DefendsName);
                     }
                     break;
                 case EnumAbilityEffect.Recovery:
@@ -289,11 +319,11 @@ public class UIBattleRoomView : BaseUIForm
 
                     if (AttackCharacter != null)
                     {
-                        HPValueChanged(ref AttackCharacter.CurrentHP, AttackCharacter.MaxHP, m_AbiilityEffectValue, UIAttackCharacter.CurrentHP_Txt, UIAttackCharacter.HP_Sli, AttackInfos[i].EffectType);
+                        HPValueChanged(ref AttackCharacter.CurrentHP, AttackCharacter.MaxHP, m_AbiilityEffectValue, UIAttackCharacter.CurrentHP_Txt, UIAttackCharacter.HP_Sli, AttackInfos[i].EffectType, m_ReportScript.m_Info_Txt, m_DefendsName);
                     }
                     else
                     {
-                        HPValueChanged(ref AttackEnemy.CurrentHP, AttackEnemy.MaxHP, m_AbiilityEffectValue, UIAttackEnemy.CurrentHP_Txt, UIAttackEnemy.HP_Sli, AttackInfos[i].EffectType);
+                        HPValueChanged(ref AttackEnemy.CurrentHP, AttackEnemy.MaxHP, m_AbiilityEffectValue, UIAttackEnemy.CurrentHP_Txt, UIAttackEnemy.HP_Sli, AttackInfos[i].EffectType, m_ReportScript.m_Info_Txt, m_DefendsName);
                     }
                     break;
                 default:
@@ -318,18 +348,59 @@ public class UIBattleRoomView : BaseUIForm
                     }
                     if (DefendsCharacter != null)
                     {
-                        HPValueChanged(ref DefendsCharacter.CurrentHP, DefendsCharacter.MaxHP, m_AbiilityEffectValue, UIDefendsCharacter.CurrentHP_Txt, UIDefendsCharacter.HP_Sli, AttackInfos[i].EffectType);
+                        HPValueChanged(ref DefendsCharacter.CurrentHP, DefendsCharacter.MaxHP, m_AbiilityEffectValue, UIDefendsCharacter.CurrentHP_Txt, UIDefendsCharacter.HP_Sli, AttackInfos[i].EffectType, m_ReportScript.m_Info_Txt, m_DefendsName);
                     }
                     else
                     {
-                        HPValueChanged(ref DefendsEnemy.CurrentHP, DefendsEnemy.MaxHP, m_AbiilityEffectValue, UIDefendsEnemy.CurrentHP_Txt, UIDefendsEnemy.HP_Sli, AttackInfos[i].EffectType);
+                        HPValueChanged(ref DefendsEnemy.CurrentHP, DefendsEnemy.MaxHP, m_AbiilityEffectValue, UIDefendsEnemy.CurrentHP_Txt, UIDefendsEnemy.HP_Sli, AttackInfos[i].EffectType, m_ReportScript.m_Info_Txt, m_DefendsName);
                     }
                     break;
 
             }
             m_ReportScript.m_DateTime_Txt.text = DateTime.Now.ToString();
+
+            SetTextHeight(m_ReportScript.m_Info_Txt);
+
             yield return new WaitForSeconds(3f);
         }
+        UIBattleReportScript m_ResultReport = CreateReport().GetComponent<UIBattleReportScript>();
+        m_ResultReport.m_DateTime_Txt.text = "";
+        m_ResultReport.m_Info_Txt.text = "";
+        if (m_ViewModel.BattleReportList.IsWin)
+        {
+            var ItemRewardList = m_ViewModel.BattleReportList.ItemRewards;
+            Log("ItemRewardList = " + ItemRewardList.Count);
+            for (int i = 0; i < ItemRewardList.Count; i++)
+            {
+                m_ResultReport.m_Info_Txt.text += string.Format(m_GetEquipment, ItemRewardList[i]);
+            }
+            var ExpRewardList = m_ViewModel.BattleReportList.ExpRewards;
+            for (int i = 0; i < ExpRewardList.Count; i++)
+            {
+                m_ResultReport.m_Info_Txt.text += string.Format(m_GetExp, m_ViewModel.characterList[i].Name, ExpRewardList[i]);
+            }
+            m_ResultReport.m_Info_Txt.text += string.Format(m_GetGold, m_ViewModel.BattleReportList.GoldRewards);
+            m_ResultReport.m_Info_Txt.text += m_SuccessResult;
+
+        }
+        else
+        {
+            m_ResultReport.m_Info_Txt.text = string.Format(m_FailureResult,m_ViewModel.BattleReportList.TeamName);
+        }
+
+        SetTextHeight(m_ResultReport.m_Info_Txt);
+
+        m_ViewModel.OnRequestBattle();
+    }
+    /// <summary>
+    /// 设置Text高度
+    /// </summary>
+    /// <param name="text"></param>
+    private void SetTextHeight(Text text)
+    {
+        var m_Height = text.preferredHeight;
+        var m_Rect = text.GetComponent<RectTransform>().rect;
+        text.GetComponent<RectTransform>().sizeDelta = new Vector2(m_Rect.width, m_Height);
     }
     /// <summary>
     /// 倒计时
@@ -344,6 +415,7 @@ public class UIBattleRoomView : BaseUIForm
             yield return null;
             txt.text = string.Format(m_FindEnemy, (int)time);
             time-= Time.deltaTime;
+            SetTextHeight(txt);
         }
         Log("倒计时结束");
         yield return new WaitForSeconds(1f);
@@ -378,6 +450,7 @@ public class UIBattleRoomView : BaseUIForm
                 "最大红量：" + m_ViewModel.characterList[i].MaxHP + "\n" +
                 "当前红量：" + m_CharacterUIInfoList[i].CurrentHP_Txt.text + "\n"
                 );
+            Log("角色初始化：HeroID = " +  m_ViewModel.characterList[i].HeroID);
         }
     }
     /// <summary>
@@ -529,6 +602,10 @@ public class UIBattleRoomViewModel
         /// 战报信息
         /// </summary>
         public List<AttackInfos> AttackInfoList = new List<AttackInfos>();
+        /// <summary>
+        /// 经验奖励
+        /// </summary>
+        public List<int> ExpRewards = new List<int>();
     }
     /// <summary>
     /// 角色集合
@@ -542,4 +619,6 @@ public class UIBattleRoomViewModel
     /// 战报集合
     /// </summary>
     public BattleReportModel BattleReportList = new BattleReportModel();
+
+    public Action OnRequestBattle;
 }
